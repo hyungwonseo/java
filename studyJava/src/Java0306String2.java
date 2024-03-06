1df6def2e977bdd994fa0a2bcd5b6ab99b5a9220
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-03-06 12:06:10 +0900

    Update Java0306String2.java

--- studyJava/src/Java0306String2.java
+++ studyJava/src/Java0306String2.java
@@ -10,12 +10,15 @@ public class Java0306String2 {
         System.out.println(str1.substring(0, 5));
 
         // 연산자를 이용하여 ':'와 '/'기준으로 분리시킴
+        // split 메소드의 리턴형은 String[] 문자열의 배열이다!!!
         String[] strArray = "Name:Michael/Man".split(":|/");
         System.out.println(Arrays.toString(strArray));
 
         // "앞뒤" 공백지우기
         String _str1 = "   abc def   ";
-        //_str1 = _str1.trim();
-        System.out.println(_str1.length());
+        _str1 = _str1.trim();
+        System.out.println(_str1);
+        // 모든 공백 지우기
+        System.out.println(_str1.replaceAll(" ", ""));
     }
 }
