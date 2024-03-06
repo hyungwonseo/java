3c8a76ab5a641ee26c1bc55843059c4923ea9df5
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-03-06 17:33:07 +0900

    Update Java0306Array.java

--- studyJava/src/Java0306Array.java
+++ studyJava/src/Java0306Array.java
@@ -51,11 +51,42 @@ public class Java0306Array {
         Scanner scan = new Scanner(System.in);
         System.out.print("0~6까지 숫자를 입력해주세요 : ");
         int num = scan.nextInt();
-
+        for (int i=0; i< week.length; i++) {
+            if (num >= 0 && num <= 6) {
+                System.out.println(week[num]);
+                break;
+            }else {
+                System.out.println("잘못 입력하셨습니다.");
+            }
+        }
 
         // 5. 사용자로부터 이름을 입력받아 아래 회원배열에 있는 이름이면
         // "~~~님 환영합니다"를 출력하고
         // 없으면 "회원가입해주세요"를 출력 (단, 대소문자 구분하지 마시오)
         String[] members = {"Steve", "Tom", "Michael", "Laura", "Jessica", "Annie"};
+        Scanner scan2 = new Scanner(System.in);
+        System.out.print("이름을 입력해주세요 : ");
+        String inputStr = scan2.nextLine();
+        boolean found = false;
+        for (int i=0; i< members.length; i++) {
+            if (members[i].equalsIgnoreCase(inputStr)) {
+                System.out.println(members[i] + "님 환영합니다");
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("회원가입해주세요");
+        }
+
+//        for (int i=0; i< members.length; i++) {
+//            if (members[i].equalsIgnoreCase(inputStr.toLowerCase())) {
+//                System.out.println(members[i] + "님 환영합니다");
+//                break;
+//            }
+//            if (i == members.length - 1) {
+//                System.out.println("회원가입해주세요");
+//            }
+//        }
     }
 }
