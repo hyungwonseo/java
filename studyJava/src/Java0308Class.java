c1edd1b0c2cf9e5adae66cf50b47ddcd75dfd6db
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-03-08 12:45:53 +0900

    Update Java0308Class.java

--- studyJava/src/Java0308Class.java
+++ studyJava/src/Java0308Class.java
@@ -10,5 +10,21 @@ public class Java0308Class {
         steve.displayAll();
         paul.displayAll();
         System.out.println(tom.getPersonalNum());
+
+        // 메소드 호출
+        int[] numberList = {12, 34, 56, 32, 84, 99, 73, 69};
+        int[] numberList2 = {123,345,235,7898,467};
+        getSumAvg(numberList);
+        getSumAvg(numberList2);
+    }
+
+    public static void getSumAvg(int[] numberList) {
+        // 7. 아래 배열의 총합과 평균을 출력
+        int sum = 0;
+        for (int i=0; i<numberList.length; i++) {
+            sum = sum + numberList[i];
+        }
+        int average = sum / numberList.length;
+        System.out.println(sum + " " + average);
     }
 }
