d372ae6a4888bbdc235ed3a1a034063d9555437e
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-03-08 17:14:03 +0900

    update

--- studyJava/src/Java0307Array.java
+++ studyJava/src/Java0307Array.java
@@ -97,6 +97,18 @@ public class Java0307Array {
             }
         }
         System.out.println(Arrays.toString(rottoList));
+
+        // 10. 양의 정수 10개를 입력받아 배열에 담고 3의 배수만 출력
+//        Scanner sc = new Scanner(System.in);
+//        int[] intArray = new int[10];
+//        for (int i=0; i< intArray.length; i++) {
+//            intArray[i] = sc.nextInt();
+//        }
+//        for (int i=0; i< intArray.length; i++) {
+//            if (intArray[i] % 3 == 0) {
+//                System.out.print(intArray[i] + " ");
+//            }
+//        }
     }
 }
 
