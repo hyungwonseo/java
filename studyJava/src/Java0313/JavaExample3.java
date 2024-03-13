913a75cd61a2c91b19b5d0b8665449c2b35a75f7
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-03-13 17:27:51 +0900

    Update JavaExample3.java

--- studyJava/src/Java0313/JavaExample3.java
+++ studyJava/src/Java0313/JavaExample3.java
@@ -1,11 +1,15 @@
 package Java0313;
 
+import java.util.Scanner;
+
 public class JavaExample3 {
+    // 오른쪽으로 이동
     public static void main(String[] args) {
 
         int [] arr = new int [] {1, 2, 3, 4, 5};
 
-        int n = 3;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
         System.out.println("Original array: ");
         for (int i = 0; i < arr.length; i++) {
@@ -13,13 +17,13 @@ public class JavaExample3 {
         }
 
         for(int i = 0; i < n; i++){
-            int j, first;
+            int j, last;
 
-            first = arr[0];  // 포인트 1
-            for(j = 0; j < arr.length-1; j++){
-                arr[j] = arr[j+1];
+            last = arr[arr.length-1];  // 포인트 1
+            for(j = arr.length-1; j > 0; j--){
+                arr[j] = arr[j-1];
             }
-            arr[j] = first;  // 포인트 2
+            arr[j] = last;  // 포인트 2
         }
         System.out.println();
 
@@ -27,4 +31,33 @@ public class JavaExample3 {
             System.out.print(arr[i] + " ");
         }
     }
+
+    // 왼쪽으로 이동
+//    public static void main(String[] args) {
+//
+//        int [] arr = new int [] {1, 2, 3, 4, 5};
+//
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//
+//        System.out.println("Original array: ");
+//        for (int i = 0; i < arr.length; i++) {
+//            System.out.print(arr[i] + " ");
+//        }
+//
+//        for(int i = 0; i < n; i++){
+//            int j, first;
+//
+//            first = arr[0];  // 포인트 1
+//            for(j = 0; j < arr.length-1; j++){
+//                arr[j] = arr[j+1];
+//            }
+//            arr[j] = first;  // 포인트 2
+//        }
+//        System.out.println();
+//
+//        for(int i = 0; i< arr.length; i++){
+//            System.out.print(arr[i] + " ");
+//        }
+//    }
 }
