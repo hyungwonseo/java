b4ef923368c2d78094d7ff114e1f561845fca723
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-03-13 14:11:27 +0900

    update

---
+++ studyJava/src/Java0313/JavaExample1.java
@@ -0,0 +1,8 @@
+package Java0313;
+
+public class JavaExample1 {
+    public static void main(String[] args) {
+        // 아래 배열중에 짝수만 출력
+        int [] arr = new int [] {1, 2, 3, 4, 5};
+    }
+}
---
+++ studyJava/src/Java0313/JavaExample2.java
@@ -0,0 +1,11 @@
+package Java0313;
+
+public class JavaExample2 {
+    public static void main(String[] args) {
+
+        // 아래 문자열의 글자수를 카운트
+        // 스페이스는 카운트하지 마시오
+        String string = "The best of both worlds";
+        int count = 0;
+    }
+}
