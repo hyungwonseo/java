07378a342c707eee51b4b5ce2198c974a41b862e
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-03-13 09:12:01 +0900

    update

--- studyJava/src/Java0312Array.java
+++ studyJava/src/Java0312Array.java
@@ -1,9 +1,24 @@
+import java.util.Arrays;
+
 public class Java0312Array {
     public static void main(String[] args) {
         // 아래 배열의 모든 값들을 다른 배열에 복사하기
         int[] intArray1 = {1,2,3,4,5};
-        int[] intArray2;
+        int[] intArray2 = new int[intArray1.length];
+        for (int i=0; i< intArray1.length; i++) {
+            intArray2[i] = intArray1[i];
+        }
+        System.out.println(Arrays.toString(intArray1));
+        System.out.println(Arrays.toString(intArray2));
 
         // 위 코드 작성한 후, git에 올려져있는 Java0312Array2.java 코드 분석!!
     }
 }
+
+
+
+
+
+
+
+
--- studyJava/src/Java0312Array2.java
+++ studyJava/src/Java0312Array2.java
@@ -1,9 +1,14 @@
+import java.util.Arrays;
+
 public class Java0312Array2 {
     public static void main(String[] args) {
         int [] arr = new int [] {1, 2, 8, 3, 2, 2, 2, 5, 1};
         int [] fr = new int [arr.length];
         int visited = -1;
         for(int i = 0; i < arr.length; i++){
+            if (fr[i] == visited) {
+                continue;
+            }
             int count = 1;
             for(int j = i+1; j < arr.length; j++){
                 if(arr[i] == arr[j]){
@@ -13,6 +18,7 @@ public class Java0312Array2 {
             }
             if(fr[i] != visited)
                 fr[i] = count;
+            System.out.println(Arrays.toString(fr));
         }
 
         for(int i = 0; i < fr.length; i++){
