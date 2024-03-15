f9aa51e5a45c789ee746656568a06356f39237ce
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-03-15 15:49:19 +0900

    Update Count.java

--- studyJava/src/Java0315/example/Count.java
+++ studyJava/src/Java0315/example/Count.java
@@ -6,6 +6,6 @@ public class Count {
         // 아래 문자열의 모음과 자음의 갯수를 각각 count 하시오
         // 모음은 a e i o u
         // 스페이스는 문자 아님
-        String str = "This is a really simple sentence";
+        String str = "This is a really simple sentence";  // 모음 10개 자음 17개
     }
 }
