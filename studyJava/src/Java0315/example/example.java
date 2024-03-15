ec3dfa72ca8e165eb535999b13dc3809bc989f14
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-03-15 17:28:23 +0900

    Update example.java

--- studyJava/src/Java0315/example/example.java
+++ studyJava/src/Java0315/example/example.java
@@ -3,18 +3,24 @@ package Java0315.example;
 import java.util.Arrays;
 
 public class example {
-    static void bubble(int[] arr) {
+    static void bubbleSort(int[] arr) {
         int n = arr.length;
         int temp = 0;
         for(int i=0; i < n; i++){
-            for(int j=1; j < (n-i); j++){
-                if(arr[j-1] > arr[j]){
-                    temp = arr[j-1];
-                    arr[j-1] = arr[j];
-                    arr[j] = temp;
+            for(int j=0; j < (n-i-1); j++){
+                if(arr[j] > arr[j+1]){
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
                 }
             }
-
+//            for(int j=1; j < (n-i); j++){
+//                if(arr[j-1] > arr[j]){
+//                    temp = arr[j-1];
+//                    arr[j-1] = arr[j];
+//                    arr[j] = temp;
+//                }
+//            }
             System.out.println(Arrays.toString(arr));
         }
     }
@@ -27,7 +33,7 @@ public class example {
         }
         System.out.println();
 
-        bubble(arr);
+        bubbleSort(arr);
 
         for(int i=0; i < arr.length; i++){
             System.out.print(arr[i] + " ");
