45dde9b1d5f2d3b9613e0b78071d12956d083bfa
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-03-25 17:05:44 +0900

    update

--- studyJava/src/Java0325/Example.java
+++ studyJava/src/Java0325/Example.java
@@ -21,6 +21,25 @@ public class Example {
         String[] names = {"Steve", "tom", "Laura", "Susie"};
         String[] departments = {"Physics", "Computer", "English", "Law"};
         int[] studentsIds = {10001, 10002, 10003, 10004};
-        
+        // 데이터베이스의 raw data(가공하지 않은 순수 데이터)를
+        // Students 객체형태로 만들고 이를 배열에 담는다.
+        Students[] students = new Students[names.length];
+        for(int i=0; i< names.length; i++) {
+            //students[i] = new Students(names[i], departments[i], studentsIds[i]);
+            students[i] = new Students();
+            students[i].setName(names[i]);
+            students[i].setDepartment(departments[i]);
+            students[i].setStudentId(studentsIds[i]);
+            System.out.println(students[i].toString());
+        }
     }
 }
+
+
+
+
+
+
+
+
+
