48193d395740b43c5aee34e0efea47eb393afc93
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-03-26 12:09:54 +0900

    update

--- studyJava/src/Java0326/ChildA.java
+++ studyJava/src/Java0326/ChildA.java
@@ -1,6 +1,6 @@
 package Java0326;
 
-public class ChildA implements A, B {
+public class ChildA extends Object implements A, B {
     @Override
     public void abc() {
         System.out.println("child of A");
--- studyJava/src/Java0326/Main.java
+++ studyJava/src/Java0326/Main.java
@@ -8,5 +8,46 @@ public class Main {
         //A.a = 100; // 오류 A.a는 상수임
         System.out.println(A.a);
         System.out.println(B.a);
+
+        A a2 = new A(){
+            public void abc() {
+                System.out.println("나는 익명 이너클래스");
+            }
+        };
+        a2.abc();
+
+        // <인터페이스 생성 방법 1>
+        // 자식클래스(ChildA)를 선언하고 해당 인터페이스를 상속하게 하면
+        // 아래와 같이 동일한 코드 내용(=메서드 오버라이딩 내용)을 계속해서 사용할 수 있음
+        ChildA c2 = new ChildA();
+        ChildA c3 = new ChildA();
+        ChildA c4 = new ChildA();
+
+        // <인터페이스 생성 방법 2>
+        // 아래와 같이 익명 이너 클래스 형식으로 생성하면,
+        // 방법1에서와 같이 재사용할 수 있는 클래스이름이 없으므로
+        // 동일한 코드 내용(=메서드 오버라이딩 내용)을 매번 복사붙여넣기해줘야 함
+        A a3 = new A(){
+            public void abc() {
+                System.out.println("나는 익명 이너클래스");
+            }
+        };
+        A a4 = new A(){
+            public void abc() {
+                System.out.println("나는 익명 이너클래스");
+            }
+        };
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
