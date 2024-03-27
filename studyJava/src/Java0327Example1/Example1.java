6debf433066486ba551711637fd5858003107d7f
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-03-27 13:41:50 +0900

    init

---
+++ studyJava/src/Java0327Example1/Example1.java
@@ -0,0 +1,21 @@
+package Java0327Example1;
+
+public class Example1 {
+    public static void main(String[] args) {
+
+        // 문제 1. 아래 2차원 배열의 모든 값의 합과 평균을 구하시오
+        int[][] arr = {
+                { 5, 5, 5, 5, 5},
+                {10,10,10,10,10},
+                {20,20,20,20,20},
+                {30,30,30,30,30}
+        };
+        int total = 0;
+        float average = 0;
+
+
+
+        System.out.println("total="+total);
+        System.out.println("average="+average);
+    }
+}
