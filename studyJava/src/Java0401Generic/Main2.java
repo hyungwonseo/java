6ca748173a420ff69cc533fa8b3a11485401d620
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-04-01 14:20:10 +0900

    update

--- studyJava/src/Java0401Generic/Main2.java
+++ studyJava/src/Java0401Generic/Main2.java
@@ -22,7 +22,17 @@ class KeyValue<K,V> {
 }
 
 public class Main2 {
-    KeyValue<String, Integer> kv1 = new KeyValue<>();
+    public static void main(String[] args) {
+        KeyValue<String, Integer> kv1 = new KeyValue<>();
+        kv1.setKey("사과");
+        kv1.setValue(1000);
+        System.out.println(kv1.getKey() + " : " + kv1.getValue());
+
+        // 두번째 제네릭을 사용하지 않더라도 Void타입을 지정해줘야 함
+        KeyValue<String, Void> kv3 = new KeyValue<>();
+        kv3.setKey("키값만 사용");
+        System.out.println("key: " + kv3.getKey());
+    }
 }
 
 
