e7a07fb3b4a2815d229e2edcf265fac2d265daf3
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-04-04 13:46:23 +0900

    update

---
+++ studyJava/src/Java0404FileIO/FileInputStream1.java
@@ -0,0 +1,38 @@
+package Java0404FileIO;
+
+import java.io.*;
+
+public class FileInputStream1 {
+    public static void main(String[] args) throws IOException {
+        // 파일 데이터 입력
+        // 1. 파일 대상 설정
+        File inFile = new File(FileObject.CURRENT + "/file1.txt");
+        if (!inFile.exists()) {
+            inFile.createNewFile();
+        }
+
+        // 2. 대상에 연결되는 stream 생성
+        InputStream is = new FileInputStream(inFile);
+
+        // 3. 데이터 읽기
+        int data;
+        while((data=is.read()) != -1) {
+            System.out.println("읽은 데이터 : " + (char)data +
+                    " 남은 바이트 : " + is.available());
+        }
+        // 4. stream 종료
+        is.close();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ studyJava/src/Java0404FileIO/FileObject.java
@@ -0,0 +1,48 @@
+package Java0404FileIO;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileObject {
+    public static final String CURRENT =
+            "C:/Users/admin/Documents/GitHub/java/studyJava/temp"; // 본인꺼
+    public static void main(String[] args) {
+        String currentPath = System.getProperty("user.dir");
+        System.out.println(currentPath);
+        // 절대경로
+        File tempDir = new File("C:/Users/admin/Documents/GitHub/java/studyJava/temp");
+        if (tempDir.exists()) {
+            System.out.println("폴더가 이미 존재합니다");
+        }else {
+            System.out.println("폴더를 생성합니다");
+            tempDir.mkdir(); // make directory
+        }
+
+        // 파일생성
+        File newFile = new File(CURRENT + "/newFile.txt");
+        if (!newFile.exists()) {
+            try {
+                newFile.createNewFile();
+            } catch (IOException e) {
+                System.out.println("IOException 예외");
+            }
+        } else {
+            System.out.println("이미 파일이 존재합니다");
+        }
+
+        // File 객체가 가르키는 대상(폴더 또는 파일)의 절대경로 확인
+        System.out.println("절대경로 위치 : " + newFile.getAbsoluteFile());
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
