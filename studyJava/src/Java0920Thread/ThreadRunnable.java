94ec359ab744266a11a92c17ea2266adbbc0810e
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-09-20 11:44:59 +0900

    update

--- studyJava/src/Java0920Thread/ThreadRunnable.java
+++ studyJava/src/Java0920Thread/ThreadRunnable.java
@@ -4,6 +4,7 @@ class SMIFileRunnable implements Runnable {
     @Override
     public void run() {
         String[] strArray = {"하나", "둘", "셋", "넷", "다섯"};
+        try {Thread.sleep(10);} catch (InterruptedException e) {}
         for (int i=0; i<strArray.length; i++) {
             System.out.println("(자막번호) " + strArray[i]);
             try {Thread.sleep(200);} catch (InterruptedException e) {}
@@ -23,6 +24,18 @@ class VideoFileRunnable implements Runnable {
 
 public class ThreadRunnable {
     public static void main(String[] args) {
-
+        // 자막 쓰레드 실행
+        Runnable smiFileRunnable = new SMIFileRunnable();
+        Thread thread1 = new Thread(smiFileRunnable);
+        thread1.start();
+        // 비디오 쓰레드 실행
+        Runnable videoFileRunnable = new VideoFileRunnable();
+        Thread thread2 = new Thread(videoFileRunnable);
+        thread2.start();
     }
 }
+
+
+
+
+
